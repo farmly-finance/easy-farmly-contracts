171aa583b49a2e172df3f916679b21a9d12cb715
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2024-10-04 10:36:48 +0300

    Update SqrtPriceX96.sol

--- src/libraries/SqrtPriceX96.sol
+++ src/libraries/SqrtPriceX96.sol
@@ -1,12 +1,14 @@
 pragma solidity 0.8.19;
 
-import {FixedPointMathLib} from "@solmate/utils/FixedPointMathLib.sol";
+import "@uniswap/v3-core/contracts/libraries/FixedPoint96.sol";
+import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 import {SafeCastUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
-
-import {LibFullMath} from "src/libraries/LibFullMath.sol";
-import {LibFixedPoint96} from "src/libraries/LibFixedPoint96.sol";
+import {FarmlyFullMath} from "./FarmlyFullMath.sol";
 
 library SqrtPriceX96 {
+    int128 private constant MIN_64x64 = -0x80000000000000000000000000000000;
+    int128 private constant MAX_64x64 = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+
     using SafeCastUpgradeable for uint256;
 
     function decodeSqrtPriceX96(
@@ -14,10 +16,10 @@ library SqrtPriceX96 {
         uint256 _token0Decimals,
         uint256 _token1Decimals
     ) internal pure returns (uint256 _priceE18) {
-        uint256 _non18Price = LibFullMath.mulDiv(
+        uint256 _non18Price = FarmlyFullMath.mulDiv(
             uint256(_sqrtPriceX96) * 1e18,
             _sqrtPriceX96,
-            LibFixedPoint96.Q96 ** 2
+            FixedPoint96.Q96 ** 2
         );
         return
             (_non18Price * (10 ** _token0Decimals)) / (10 ** _token1Decimals);
@@ -28,9 +30,49 @@ library SqrtPriceX96 {
         uint256 _token0Decimals,
         uint256 _token1Decimals
     ) internal pure returns (uint160 _sqrtPriceX96) {
-        uint256 _sqrt = FixedPointMathLib.sqrt(
+        uint256 _sqrt = FarmlyFullMath.sqrt(
             (_priceE18 * (10 ** _token1Decimals)) / (10 ** _token0Decimals)
         );
-        return ((_sqrt * LibFixedPoint96.Q96) / 1e9).toUint160();
+        return uint160((_sqrt * FixedPoint96.Q96) / 1e9);
+    }
+
+    function ABDKMath64x64div(
+        int128 x,
+        int128 y
+    ) internal pure returns (int128) {
+        unchecked {
+            require(y != 0);
+            int256 result = (int256(x) << 64) / y;
+            require(result >= MIN_64x64 && result <= MAX_64x64);
+            return int128(result);
+        }
+    }
+
+    function divRound(
+        int128 x,
+        int128 y
+    ) internal pure returns (int128 result) {
+        int128 quot = ABDKMath64x64div(x, y);
+        result = quot >> 64;
+
+        // Check if remainder is greater than 0.5
+        if (quot % 2 ** 64 >= 0x8000000000000000) {
+            result += 1;
+        }
+    }
+
+    function nearestUsableTick(
+        int24 tick_,
+        uint24 tickSpacing
+    ) internal pure returns (int24 result) {
+        result =
+            int24(divRound(int128(tick_), int128(int24(tickSpacing)))) *
+            int24(tickSpacing);
+
+        if (result < TickMath.MIN_TICK) {
+            result += int24(tickSpacing);
+        } else if (result > TickMath.MAX_TICK) {
+            result -= int24(tickSpacing);
+        }
     }
 }
