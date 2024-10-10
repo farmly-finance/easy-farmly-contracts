f43eb5f8fea6a87e5b536f1cec4d6372d6c990cd
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2024-10-10 16:13:27 +0300

    Create IFarmlyUniV3Executor.sol

---
+++ src/interfaces/IFarmlyUniV3Executor.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.8.13;
+
+interface IFarmlyUniV3Executor {
+    struct PositionInfo {
+        int24 tickLower;
+        int24 tickUpper;
+        uint amount0Add;
+        uint amount1Add;
+    }
+
+    struct SwapInfo {
+        address tokenIn;
+        address tokenOut;
+        uint256 amountIn;
+        uint256 amountOut;
+        uint160 sqrtPriceX96;
+    }
+
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function poolFee() external view returns (uint24);
+
+    function tickSpacing() external view returns (uint24);
+
+    function latestTokenId() external view returns (uint256);
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4);
+
+    function getAmountsForAdd(
+        PositionInfo memory positionInfo
+    )
+        external
+        view
+        returns (
+            SwapInfo memory swapInfo,
+            uint256 amount0Add,
+            uint256 amount1Add
+        );
+
+    function positionAmounts()
+        external
+        view
+        returns (uint256 amount0, uint256 amount1);
+
+    function positionFees()
+        external
+        view
+        returns (uint256 amount0, uint256 amount1);
+}
