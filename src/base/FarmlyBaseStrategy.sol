a4d3b8c9160888491446391eb5658b1968944240
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2025-01-19 16:21:47 +0300

    Create FarmlyBaseStrategy.sol

---
+++ src/base/FarmlyBaseStrategy.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.8.13;
+
+import {IFarmlyBaseStrategy} from "../interfaces/IFarmlyBaseStrategy.sol";
+import {IUniV3Reader} from "../interfaces/IUniV3Reader.sol";
+
+abstract contract FarmlyBaseStrategy is IFarmlyBaseStrategy {
+    /// @inheritdoc IFarmlyBaseStrategy
+    uint256 public override latestLowerPrice;
+    /// @inheritdoc IFarmlyBaseStrategy
+    uint256 public override latestUpperPrice;
+    /// @inheritdoc IFarmlyBaseStrategy
+    uint256 public override latestTimestamp;
+    /// @inheritdoc IFarmlyBaseStrategy
+    IUniV3Reader public override uniV3Reader =
+        IUniV3Reader(0x0000000000000000000000000000000000000000);
+    /// @inheritdoc IFarmlyBaseStrategy
+    address public override uniswapPool =
+        0x0000000000000000000000000000000000000000;
+
+    /// @notice NotImplemented error
+    error NotImplemented();
+
+    /*
+    /// @inheritdoc IFarmlyBaseStrategy
+    function beforeRebalance() external virtual {
+        revert NotImplemented();
+    }
+    /// @inheritdoc IFarmlyBaseStrategy
+    function afterRebalance() external virtual {
+        revert NotImplemented();
+    }
+
+    /// @inheritdoc IFarmlyBaseStrategy
+    function beforeDeposit(uint256 _amount) external virtual {
+        revert NotImplemented();
+    }
+    /// @inheritdoc IFarmlyBaseStrategy
+    function afterDeposit(uint256 _amount) external virtual {
+        revert NotImplemented();
+    }
+
+    /// @inheritdoc IFarmlyBaseStrategy
+    function beforeWithdraw(uint256 _amount) external virtual {
+        revert NotImplemented();
+    }
+    /// @inheritdoc IFarmlyBaseStrategy
+    function afterWithdraw(uint256 _amount) external virtual {
+        revert NotImplemented();
+    }
+    */
+}
