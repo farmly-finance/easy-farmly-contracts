0195fb08322fb62c209244b5e08714738279e1d5
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2025-01-29 21:24:19 +0300

    Create MockERC20Token.sol

---
+++ test/mocks/MockERC20Token.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20Token is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+}
