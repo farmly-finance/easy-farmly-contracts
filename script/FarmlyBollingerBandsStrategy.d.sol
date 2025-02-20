34195cf9022ef1d29142407f5111b18bd9bd9d42
İbrahim Keçici <ibrahimkecici4116@gmail.com>
2025-02-20 19:50:58 +0300

    deploy new bb

--- script/FarmlyBollingerBandsStrategy.d.sol
+++ script/FarmlyBollingerBandsStrategy.d.sol
@@ -10,10 +10,10 @@ contract DeployFarmlyBollingerBandsStrategy is Script {
 
         vm.startBroadcast(deployerPrivateKey);
 
-        address token0PriceFeed = 0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1;
+        address token0PriceFeed = 0x0FB99723Aee6f420beAD13e6bBB79b7E6F034298;
         address token1PriceFeed = 0xd30e2101a97dcbAeBCBC04F14C3f624E67A35165;
         uint16 ma = 20;
-        uint16 std = 3;
+        uint16 std = 2;
         uint256 period = 1 hours;
         uint256 rebalanceThreshold = 500;
 
